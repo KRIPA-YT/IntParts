a37e26a86fa7853be3f6f91a7406e4e2489ef7d2
Amethyst <hbqacc@gmail.com>
2025-03-09 15:07:47 +0100

    Added indefinite Integral part

--- main.py
+++ main.py
@@ -42,4 +42,54 @@ class PartialInt(Slide):
         self.play(Write(question_marks))
         self.next_slide()
 
+        self.play(Unwrite(integral_sum), Unwrite(integral_product), Unwrite(solved_integrals), Unwrite(question_marks))        
+        title = Text("Partielle Integration", gradient=(ManimColor("#AA00FF"), ManimColor("#0066FF"))).scale(2)
+        subtitle = Text("GFS von Amy", color=GRAY).scale(1)
+        subtitle.move_to(DOWN * 1.5)
+        self.play(Write(title), Write(subtitle))
+        self.next_slide()
+
+        integral_text = MathTex(r"\int f(x) \mathrm{d}x := \text{Stammfunktionen von } f(x)", color=GRAY, tex_to_color_map={
+            r"\int": RED,
+            r"f(x)": BLUE,
+            r"\mathrm{d}x": RED,
+        }).scale(1.25)
+
+        self.play(Unwrite(title), Unwrite(subtitle))
+        self.play(Write(integral_text))
+        self.next_slide()
+
+        integral = MathTex(r"\int", r"f'(x)", r"\mathrm{d}x", r":=", r"f(x)", r"+", r"C", tex_to_color_map={
+            r"\int": RED,
+            r"f'(x)": BLUE,
+            r"\mathrm{d}x": RED,
+            r"f(x)": BLUE,
+            r"C": RED,
+        })
+
+        integral_text_copy = integral_text.copy().move_to(UP * 3)
+        integral.next_to(integral_text_copy, DOWN, buff = 0.5)
+        self.play(Transform(integral_text, integral_text_copy))
+        self.play(Write(integral))
+        self.next_slide()
+
+        integral_squared = MathTex(r"\int 2x \mathrm{d}x = x^2 + C", tex_to_color_map={
+            r"\int": RED,
+            r"2x": BLUE,
+            r"\mathrm{d}x": RED,
+            r"x^2": BLUE,
+            r"C": RED,
+        })
+        integral_cos = MathTex(r"\int \cos x \mathrm{d}x = \sin x + C", tex_to_color_map={
+            r"\int": RED,
+            r"\cos x": BLUE,
+            r"\mathrm{d}x": RED,
+            r"\sin x": BLUE,
+            r"C": RED,
+        })
         
+        integral_squared.move_to(DOWN * 2 + LEFT * 3)
+        integral_cos.move_to(DOWN * 2 + RIGHT * 3)
+
+        self.play(Transform(integral.copy(), integral_squared), Transform(integral.copy(), integral_cos))
+        self.next_slide()
